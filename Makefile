f9cf1b7547408e354869f8e9f8ea794d6037f14b
acoshift <acoshift@gmail.com>
2018-09-19 13:03:05 +0700

    fix cmd, bump version

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
 IMAGE=acoshift/revprox
-TAG=1.1.0
+TAG=1.2.0
 GOLANG_VERSION=1.11
 REPO=github.com/acoshift/revprox
 
-revprox: main.go
-	go get -v
-	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-w -s' -o revprox ./main.go
+revprox:
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-w -s' -o revprox cmd/revprox/main.go
 
 build-docker:
 	docker pull golang:$(GOLANG_VERSION)
@@ -13,7 +12,7 @@ build-docker:
 	docker build --pull -t $(IMAGE):$(TAG) .
 
 build-linux:
-	env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o revprox -a -ldflags '-w -s' main.go
+	env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o revprox -a -ldflags '-w -s' cmd/revprox/main.go
 	chmod +x revprox
 
 push-docker: clean build-docker
@@ -23,12 +22,4 @@ dev:
 	go run cmd/revprox/main.go
 
 clean:
-	rm -f revprox revprox.tar.gz
-
-compress:
-	tar czf revprox.tar.gz revprox
-
-upload:
-	gsutil -h "Cache-Control: public, max-age=30" cp -a public-read revprox.tar.gz gs://acoshift/
-
-deploy-gcs:	clean build-linux compress upload
+	rm -f revprox
---
+++ cmd/revprox/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/acoshift/revprox"
+)
+
+func main() {
+	var (
+		addr          = flag.String("addr", ":8080", "listen address and port")
+		target        = flag.String("target", "http://localhost", "target upstream url")
+		host          = flag.String("host", "", "override the host header sent to the upstream")
+		userAgent     = flag.String("user-agent", "", "override the user-agent header sent to the upstream")
+		path          = flag.String("path", "", "override the request path")
+		extraRequest  = flag.String("extra-request", "", "extra comma-separated request headers to send to the upstream")
+		extraResponse = flag.String("extra-response", "", "extra comma-separated response headers to send back to the client")
+		authRealm     = flag.String("auth-realm", "Restricted", "http basic auth realm (frontend)")
+		authUsername  = flag.String("auth-user", "admin", "http basic auth username (frontend)")
+		authPassword  = flag.String("auth-pass", "", "http basic auth password (frontend)")
+		stripURI      = flag.Bool("strip-uri", false, "strip the href path")
+		hideServer    = flag.Bool("hide-server", false, "hide the upstream server in responses")
+		noCache       = flag.Bool("no-cache", false, "send no-cache header in responses")
+		accessLog     = flag.Bool("access-log", false, "enable access logging")
+	)
+
+	flag.Parse()
+
+	fmt.Printf("revprox %s\n", revprox.Version)
+
+	// target url validation
+	origin, err := url.Parse(*target)
+	if err != nil {
+		log.Fatalf("parse url; %v", err)
+	}
+
+	proxy := &revprox.Proxy{
+		Origin:        origin,
+		Host:          *host,
+		UserAgent:     *userAgent,
+		Path:          *path,
+		ExtraRequest:  *extraRequest,
+		ExtraResponse: *extraResponse,
+		AuthRealm:     *authRealm,
+		AuthUsername:  *authUsername,
+		AuthPassword:  *authPassword,
+		StripURI:      *stripURI,
+		HideServer:    *hideServer,
+		NoCache:       *noCache,
+		AccessLog:     *accessLog,
+	}
+
+	log.Printf("start revprox on %s\n", *addr)
+	if err := http.ListenAndServe(*addr, proxy); err != nil {
+		log.Fatalf("revprox; %v", err)
+	}
+}
